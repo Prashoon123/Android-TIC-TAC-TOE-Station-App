a4d87ab8c360cb6a0e682fc1f362c7e3b38b2bea
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-12-03 17:24:30 +0530

    First Commit

---
+++ app/src/main/java/app/web/prashoonapps/tic_tac_toe_station/MainActivity.java
@@ -0,0 +1,22 @@
+package app.web.prashoonapps.tic_tac_toe_station;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://prashoon123.github.io/TIC-TAC-TOE-Station/");
+    }
+}
